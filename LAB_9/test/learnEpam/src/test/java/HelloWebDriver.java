120c2997ff2c0f181cf560365d9a3f06a1e20497
Lordan0001 <4belstu@mail.ru>
2022-11-14 22:49:51 +0300

    before testNG

---
+++ LAB_9/test/learnEpam/src/test/java/HelloWebDriver.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class HelloWebDriver {
+    public static void main(String[] args) throws InterruptedException {
+
+//        WebDriver driver = new EdgeDriver();
+//        driver.get("https://www.selenium.dev/");
+////        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+////        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+////        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//
+//       WebElement searchInput = driver.findElement(By.className("navbar-toggler"));
+//       searchInput.click();
+//        WebElement searchInput2 = driver.findElement(By.className("DocSearch DocSearch-Button"));
+//        searchInput2.click();
+//
+//       searchInput.sendKeys("selenium java");
+//       WebElement serachBtn = driver.findElement(By.xpath("//*[enterkeyhint='go']"));
+//       serachBtn.click();
+//        Thread.sleep(2000);
+//        driver.quit();
+
+//search test
+//        WebDriver driver = new EdgeDriver();
+//        driver.get("https://rockbastion.by/");
+//        WebElement searchInput = driver.findElement(By.name("search"));
+//        searchInput.click();
+//        searchInput.sendKeys("украшения");
+//        searchInput.sendKeys(Keys.ENTER);
+//        Thread.sleep(2000);
+//        driver.quit();
+
+
+
+
+//        WebDriver driver = new EdgeDriver();
+//        driver.get("https://rockbastion.by/");
+//        WebElement searchInput = driver.findElement(By.xpath("/html/body/div[4]/header/div/div/div[2]/div/input"));
+//        searchInput.click();
+//        searchInput.sendKeys("украшения");
+//        searchInput.sendKeys(Keys.ENTER);
+//        Thread.sleep(2000);
+//        driver.quit();
+
+    }
+}
